4040d454b055e434d78b1bf4e0db2aaeba523db0
eld16 <evan.rahardja97@gmail.com>
2020-04-06 08:30:29 +0700

    Updates

---
+++ precache-manifest.83fb59457cf2e334e81f0b27471d8340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a635b982f5d8eac50cc2084ff9881bd",
+    "url": "/evan-rhdja/react-simple-calculator/index.html"
+  },
+  {
+    "revision": "b2f3a910f0a836f544dc",
+    "url": "/evan-rhdja/react-simple-calculator/static/css/main.fa8ed7d0.chunk.css"
+  },
+  {
+    "revision": "980798c24abda8d061e2",
+    "url": "/evan-rhdja/react-simple-calculator/static/js/2.7ad99a1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/evan-rhdja/react-simple-calculator/static/js/2.7ad99a1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f3a910f0a836f544dc",
+    "url": "/evan-rhdja/react-simple-calculator/static/js/main.f03479b9.chunk.js"
+  },
+  {
+    "revision": "afb856ceff92a624fca3",
+    "url": "/evan-rhdja/react-simple-calculator/static/js/runtime-main.a811e23b.js"
+  }
+]);
